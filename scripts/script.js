5564e288ea666d00eeada7e52ee3220a25835274
Christopher Mertin <cmertin@cs.utah.edu>
2016-11-16 19:09:43 -0700

    Reads in all data into JSON format

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 var percent_width = 0.75;
 var percent_height = 0.80;
+var states_data = [];
+var counties_data = [];
+
 
 var mapSVG = document.getElementById("#map");
 
-console.log(window.innerWidth);
-console.log(window.innerHeight);
 var window_width = window.innerWidth;
 var window_height = window.innerHeight;
 
@@ -39,23 +40,27 @@ svg.append("rect")
 
 var g = svg.append("g");
 
-d3.json("/Data/us.json", function(error, us) {
-  if (error) throw error;
+d3.json("/Data/us.json", function(error, us)
+  {
+    if (error) throw error;
 
-  g.selectAll("path")
-      .data(topojson.feature(us, us.objects.states).features)
-    .enter().append("path")
-      .attr("d", path)
-      .attr("class", "feature")
-      .on("click", clicked);
+    g.selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+      .enter().append("path")
+        .attr("d", path)
+        .attr("class", "feature")
+        .on("click", clicked);
 
-  g.append("path")
-      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-      .attr("class", "mesh")
-      .attr("d", path);
-});
+    g.append("path")
+        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+        .attr("class", "mesh")
+        .attr("d", path);
+  });
 
-function clicked(d) {
+ReadData();
+
+function clicked(d)
+{
   if (active.node() === this) return reset();
   active.classed("active", false);
   active = d3.select(this).classed("active", true);
@@ -74,7 +79,8 @@ function clicked(d) {
   d3.select('body').select('svg').select('rect').call(zoom);
 }
 
-function reset() {
+function reset()
+{
   active.classed("active", false);
   active = d3.select(null);
 
@@ -84,17 +90,133 @@ function reset() {
       .attr('transform', 'translate(' + translate + ') scale(' + scale + ')');
 }
 
-// function zoomed() {
-//   g.attr("transform", d3.event.transform);
-//   g.style("stroke-width", 1.5 / d3.event.scale + "px");
-// }
-
 // If the drag behavior prevents the default click,
 // also stop propagation so we don’t click-to-zoom.
-function stopped() {
+function stopped()
+{
   if (d3.event.defaultPrevented) d3.event.stopPropagation();
 }
 
-function chooseData(){
-  // will write this later
+function chooseData()
+{
+
+}
+
+function ReadData()
+{
+  var base_labels = ["Bachelors", "Divorced", "Graduate/Professional", "HS/GED", "Married", "Never Married", "No HS", "Population", "Separated", "Some College", "Widowed"];
+  var other_labels = ["Asian", "Black", "Native Hawaiian and Other Paciffic Islander", "Other", "Total", "Two or More", "White"];
+  var ages = [];
+  var years = ["2010", "2011", "2012", "2013", "2014", "2015"];
+  var geo_ = ["States", "Counties"];
+  var genders = ["M", "F"];
+
+  // Create a list of ages {18, 19, 20,...,63, 64, 65+}
+  for(var i = 18; i <= 65; i++)
+  {
+    if(i == 65)
+    {
+      ages.push(i + '+');
+    }
+    else
+    {
+      ages.push(i + '');
+    }
+  }
+
+  var BASE_DIR = "/Data/" + geo_[0] + "/";
+  var FILE_EXT = "_ACS.csv";
+
+  ReadStates(BASE_DIR, FILE_EXT, years, geo_, genders, ages, base_labels, other_labels);
+
+  BASE_DIR = "/Data/" + geo_[1] + "/";
+  ReadCounties(BASE_DIR, FILE_EXT, years, geo_, genders, ages, base_labels, other_labels);
+}
+
+function ReadStates(BASE_DIR, FILE_EXT, years, geo_, genders, ages, base_labels, other_labels)
+{
+  var year_data = []
+  for(var yr = 0; yr < years.length; yr++)
+  {
+    var csv_file = BASE_DIR + years[yr] + FILE_EXT;
+    var temp = 0
+    d3.csv(csv_file, function(d)
+    {
+      var geo_data = [];
+      for(var ind = 0; ind < d.length; ind++)
+      {
+        var local_geo = d[ind].Geo;
+        var gen_data = [];
+        for(var gen = 0; gen < genders.length; gen++)
+        {
+          var age_data = [];
+          for(var i = 0; i < ages.length; i++)
+          {
+            var label_data = [];
+            for(var j = 0; j < base_labels.length; j++)
+            {
+              var csv_key = ages[i] + "_" + genders[gen] + "_" + base_labels[j];
+              label_data[base_labels[j]] = +d[ind][csv_key];
+            }
+            age_data[ages[i]] = label_data;
+          }
+          gen_data[genders[gen]] = age_data;
+        }
+        geo_data[local_geo] = gen_data;
+
+        // Other labels for region done here
+        for(var i = 0; i < other_labels.length; i++)
+        {
+          geo_data[local_geo][other_labels[i]] = +d[ind][other_labels[i]];
+        }
+      }
+      year_data[years[temp]] = geo_data;
+      temp = temp + 1;
+    });
+  }
+  states_data = year_data;
+}
+
+function ReadCounties(BASE_DIR, FILE_EXT, years, geo_, genders, ages, base_labels, other_labels)
+{
+  var year_data = []
+  for(var yr = 0; yr < years.length; yr++)
+  {
+    var csv_file = BASE_DIR + years[yr] + FILE_EXT;
+    var temp = 0
+    d3.csv(csv_file, function(d)
+    {
+      var geo_data = [];
+      for(var ind = 0; ind < d.length; ind++)
+      {
+        var local_geo = d[ind].Geo;
+        var gen_data = [];
+        for(var gen = 0; gen < genders.length; gen++)
+        {
+          var age_data = [];
+          for(var i = 0; i < ages.length; i++)
+          {
+            var label_data = [];
+            for(var j = 0; j < base_labels.length; j++)
+            {
+              var csv_key = ages[i] + "_" + genders[gen] + "_" + base_labels[j];
+              label_data[base_labels[j]] = +d[ind][csv_key];
+            }
+            age_data[ages[i]] = label_data;
+          }
+          gen_data[genders[gen]] = age_data;
+        }
+        geo_data[local_geo] = gen_data;
+
+        // Other labels for region done here
+        for(var i = 0; i < other_labels.length; i++)
+        {
+          geo_data[local_geo][other_labels[i]] = +d[ind][other_labels[i]];
+        }
+      }
+      year_data[years[temp]] = geo_data;
+      temp = temp + 1;
+    });
+  }
+  counties_data = year_data;
 }
