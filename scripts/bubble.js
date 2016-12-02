bf14d35f58217af72847e6eed22eb8713cf8ce45
Christopher Mertin <cmertin@cs.utah.edu>
2016-12-01 18:07:19 -0700

    Working moving, radius, and coloring

--- scripts/bubble.js
+++ scripts/bubble.js
@@ -2,24 +2,24 @@ var simulation = null;
 var forceStrength = 0.03;
 var midHeight = null;
 var xScale = null;
-var margin = {"left":60, "right": 70, "top":10, "bottom":30};
+var margin = {"left":100, "right": 70, "top":10, "bottom":30};
 var height = 400;
 var width = 1400;
 
-var state_tip = d3.tip()
-                  .attr("class", "d3-tip")
-                  .offset([-8, 0])
-                  .html(function(d) {
-                     var value = (+d.val).toFixed(2) + '%';
-                     var pop = NumberWithCommas(d.pop);
-                     var str = '<div class="state-tooltip-title">' +
-                     d.Geo + '</div>'
-                     + '<span class=state-label-P>Percentage: </span>'
-                     + '<span class=state-value-P>' + value + '</span><br/>'
-                     + '<span class=state-label-P>Matching Population: </span>'
-                     + '<span class=state-value-P>' + pop + '</span>';
-                     return str;
-                 });
+var state_bubbles = d3.tip()
+                      .attr("class", "d3-tip")
+                      .offset([-8, 0])
+                      .html(function(d) {
+                         var value = (+d.val).toFixed(2) + '%';
+                         var pop = NumberWithCommas(d.pop);
+                         var str = '<div class="state-tooltip-title">' +
+                         d.Geo + '</div>'
+                         + '<span class=state-label-P>Percentage: </span>'
+                         + '<span class=state-value-P>' + value + '</span><br/>'
+                         + '<span class=state-label-P>Matching Population: </span>'
+                         + '<span class=state-value-P>' + pop + '</span>';
+                         return str;
+                     });
 
 function CreateNodes(data)
 {
@@ -197,59 +197,57 @@ function UpdateChart(year)
 
   simulation.stop();
 
-  function chart()
+  var nodes = CreateNodes(state_array);
+
+  svg = d3.select("#bubble-chart");
+
+  var oldBubbles = d3.select("#bubble-chart").selectAll(".bubble");
+
+  for(var i = 0; i < nodes.length; i++)
   {
-    var nodes = CreateNodes(state_array);
+    var old = oldBubbles.filter(function(d) {return nodes[i].id == d.id;}).node();
+    nodes[i].x = parseFloat(old.attributes.cx.value);
+    nodes[i].y = parseFloat(old.attributes.cy.value);
+  }
 
-    svg = d3.select("#bubble-chart");
+  oldBubbles.transition().duration(1500).attr('r', function(d) {
+                                                  var t = this;
+                                                  var local = (nodes.filter(function(d) {return t.id == d.id}))[0];
+                                                  return local["radius"];})
+                                        .attr("fill", function(d){
+                                                  var t = this;
+                                                  var local = (nodes.filter(function(d) {return t.id == d.id}))[0];
+                                                  return color(+local["value"])
+                                        });
 
-    var temp = d3.select("#bubble-chart").selectAll(".bubble");
+  // Bind nodes data to what will become DOM elements to represent them.
+  bubbles = svg.selectAll('.bubble')
+               .data(nodes, function (d, i) {return d.id;});
 
-    //d3.selectAll(".bubble").data(temp, function(d) {console.log(d)});
-
-    // Bind nodes data to what will become DOM elements to represent them.
-    bubbles = svg.selectAll('.bubble')
-                 .data(nodes, function (d, i) {return d.id;});
-
-    // Create new circle elements each with class `bubble`.
-    // There will be one circle.bubble for each object in the nodes array.
-    var bubblesE = bubbles.enter().append("circle")
-                          .classed("bubble", true)
-                          .attr("id", function(d) {return d.id;})
-                          .attr("pop", function(d) {return d.pop;})
-                          .attr("val", function(d) {return d.value;})
-                          .attr('r', 0)
-                          .attr('fill', function(d) {return color(d.value);});
+  // Create new circle elements each with class `bubble`.
+  // There will be one circle.bubble for each object in the nodes array.
+  var bubblesE = bubbles.enter().append("circle")
+                        .classed("bubble", true)
+                        .attr("id", function(d) {return d.id;})
+                        .attr("pop", function(d) {return d.pop;})
+                        .attr("val", function(d) {return d.value;})
+                        .attr('r', function(d) {return radiusScale(d.value);})
+                        .attr('fill', function(d) {return color(d.value);});
 //                          .on("mouseover", state_tip.show)
 //                          .on("mouseout", state_tip.hide);
 
-    bubbles = bubbles.merge(bubblesE);
-
-    // Fancy transition to make bubbles appear, ending with the
-    // correct radius
-    bubbles.transition()
-           .duration(2000)
-           .attr('r', function (d) { return d.radius; });
-
-    // Set the simulation's nodes to our newly created nodes array.
-    simulation.nodes(nodes);
-
-    MoveBubbles();
-  }
+  bubbles = bubbles.merge(bubblesE);
 
 
-  /*
-   * Callback function that is called after every tick of the
-   * force simulation.
-   * Here we do the acutal repositioning of the SVG circles
-   * based on the current x and y values of their bound node data.
-   * These x and y values are modified by the force simulation.
-   */
+  simulation.nodes(nodes);
+
+  MoveBubbles();
+
+
   function ticked()
   {
       bubbles.attr('cx', function (d) {return d.x; })
              .attr('cy', function (d) {return d.y; });
   }
 
-  chart();
 }
